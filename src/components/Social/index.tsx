5c920968327d9ef18aead7933ece8ce60f8e7d7c
vfrancadev <viniciuslek159@outlook.com>
2025-07-06 12:58:14 -0300

    Corrige tipagem do componente Social

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -1,20 +1,20 @@
-import {ReactNode } from "react";
+// Corrigir a importação de tipo
+import type { ReactNode } from "react";
 
-
-interface SocialProps{
-    url: string;
-    children: ReactNode;
+interface SocialProps {
+  url: string;
+  children: ReactNode;
 }
 
-
-export function Social({url, children}){
-    return (
-
-        <a href={url}
-        rel="noopener noreferrer"
-        target="_blank"
-        >
-            {children}
-        </a>
-    )
+// Anotar o tipo do parâmetro da função
+export function Social({ url, children }: SocialProps) {
+  return (
+    <a
+      href={url}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      {children}
+    </a>
+  );
 }
